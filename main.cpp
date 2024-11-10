d1e3c6dbdd1cad25776d573b7278e4b551b8d0b8
J0k3s0nY0u <my.dev.vic@gmail.com>
2024-11-10 21:45:20 +0100

    added sfml

--- main.cpp
+++ main.cpp
@@ -49,5 +49,30 @@ int main() {
 	sanitizingRobot.exploreAndSanitize();
 	sanitizingRobot.returnToBase();
 
+	std::vector<std::vector<int>> grid = {
+		{0, 0, 0, 1, 0},
+		{0, 1, 0, 1, 0},
+		{0, 1, 0, 0, 0},
+		{0, 0, 0, 1, 1},
+		{1, 1, 0, 0, 0}
+	};
+
+	PathPlanner planner(grid);
+	Position start = { 0, 0 };
+	Position target = { 4, 4 };
+
+	std::vector<Position> path = planner.findPath(start, target);
+
+	if (!path.empty()) {
+		std::cout << "Path found:\n";
+		for (const auto& pos : path) {
+			std::cout << "(" << pos.x << ", " << pos.y << ") ";
+		}
+		std::cout << "\n";
+	}
+	else {
+		std::cout << "No path found.\n";
+	}
+
 	return 0;
 }
